7ae8ff5bc636d15e2d979c3b4f2a24978cd32361
Chance <chance.felkel@gmail.com>
2023-10-12 15:12:23 -0400

    finished html, started functionality for buttons/display

--- index.js
+++ index.js
@@ -2,3 +2,22 @@ const add = (x, y) => x + y;
 const subtract = (x, y) => x - y;
 const multiply = (x, y) => x * y;
 const divide = (x, y) => x / y;
+
+let firstNum = 0;
+let secondNum = 0;
+let operator = "";
+
+const operate = (op, x, y) => op(x, y);
+
+let displayBottom = document.querySelector(".bottom-row");
+const allNums = document.querySelectorAll(".number");
+let allOperators = document.querySelectorAll(".operator");
+// update the displayBottom.textContent when a button with class number or
+// operator is pressed
+
+allNums.forEach((node) =>
+  node.addEventListener(
+    "click",
+    () => (displayBottom.textContent += node.textContent)
+  )
+);
